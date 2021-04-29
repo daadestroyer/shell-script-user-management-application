250a7920a068c2cf85addbe97d8f93e682f5b12b
daadestroyer <nigamshubham2000@gmail.com>
2021-04-29 19:34:59 +0530

    user-search module completed...

--- user-management-application.sh
+++ user-management-application.sh
@@ -11,7 +11,7 @@ add_user(){
         read -p "First Name : " fname
         read -p "Last Name : " lname
         read -p "User Id : " uid
-        count=$(cat users.dat | cut -d ":" -f 1 | grep $uid | wc -l)
+        count=$(cat users.dat | cut -d ":" -f 1 | grep -w $uid | wc -l)
         
         # if userid already exist
         if [ $count -ne 0 ]; then 
@@ -32,7 +32,7 @@ add_user(){
             return 2
         fi
         read -p "zip : " zip 
-        echo "$uid:$fname:$lname:$zip" >> users.dat
+        echo "$uid:$pswd:$fname:$lname:$zip" >> users.dat
         echo "---------------------------------"
         echo "Users added sucessfully...."
         echo "---------------------------------"     
@@ -42,6 +42,68 @@ add_user(){
           
 }
 
+search_user(){
+    read -p "Enter user id to search : " uid
+    count=$(cat users.dat | cut -d ":" -f 1 | grep -w $uid | wc -l)
+    if [ $count -eq 0 ]; then
+        echo "---------------------------------"
+        echo "User Id : $uid does'nt exist"
+        echo "---------------------------------"
+        return 3
+    fi
+    read -p "Enter user password to search : " upwd
+    count=$(cat users.dat | grep -w $uid | cut -d ":" -f 2 |grep -w $upwd | wc -l)
+    if [ $count -eq 0 ]; then
+        echo "---------------------------------"
+        echo "Invalid Password"
+        echo "---------------------------------"
+        return 4
+    fi
+    
+    echo
+    available="no"
+    # count=$(cat users.dat | cut -d ":" -f 1 | grep $uid | wc -l)
+    # # if userid doesn't exist
+    #     if [ $count -eq 0 ]; then 
+    #         echo "---------------------------------"
+    #         echo "User Id : $uid doesn't exist"
+    #         echo "---------------------------------"
+    #         return 3
+    #     fi
+    # count=$(cat users.dat | grep $uid | cut -d ":" -f 2 |grep $upwd | wc -l)
+    # # if upwd doesn't exist
+    #     if [ $count -eq 0 ]; then 
+    #         echo "---------------------------------"
+    #         echo "Invalid Password"
+    #         echo "---------------------------------"
+    #         return 4
+    #     fi
+        
+        while read line
+        do
+            fuid=$(echo $line | cut -d ":" -f1)
+            fpwd=$(echo $line | cut -d ":" -f2)
+            if [ $uid = $fuid -a $upwd = $fpwd ]; then
+                available="yes"
+                echo "Complete User Information"
+                echo "--------------------------"
+                echo "User Id : $(echo $line | cut -d ":" -f 1)"
+                echo "User Password : $(echo $line | cut -d ":" -f 2)"
+                echo "First Name : $(echo $line | cut -d ":" -f 3)"
+                echo "Last Name : $(echo $line | cut -d ":" -f 4)"
+                echo "ZIP Code : $(echo $line | cut -d ":" -f 5)"
+                echo
+                echo
+                break
+                
+            fi
+            done < users.dat
+            if [ $available = no ]; then
+                echo "---------------------------------"
+                echo "User id or Password not matched"  
+                echo "---------------------------------"  
+            fi
+}
 
 
 echo "****************************************"
